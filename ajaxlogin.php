63c3c2fa6c8ed70ebbf577c9b4d64024acf7d130
ankitbhowmick007 <ankitbhowmick03@gmail.com>
2017-09-04 13:05:51 +0530

    Create ajaxlogin.php

---
+++ ajaxlogin.php
@@ -0,0 +1,9 @@
+<?php
+if($_POST['id1'] && $_POST['id2']){
+  //do whatever for login auth
+  //if everything ok
+  $arr=array("foo"=>"bar");
+  //else something wrong
+  $arr=array("error"=>"custom message or whatever you want");
+}
+?>
